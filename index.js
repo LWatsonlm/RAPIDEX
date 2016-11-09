296d40a7905f3b046137237bc31d099c3f9d76b4
Tim Foley <thetimfoley@gmail.com>
2016-11-09 15:09:53 -0500

    OUR API IS ALIIIIIVE

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express     = require("express");
 var Pokedex     = require("pokedex-promise-v2")
-// var mongoose    = require("./db/connection");
+var mongoose    = require("./db/connection");
+var Pokemon     = mongoose.model("Pokemon")
 var P = new Pokedex()
 var app = express()
 
@@ -12,5 +13,8 @@ app.listen(app.get("port"), function(){
 });
 
 app.get("/api/pokemon", (req, res) => {
-
+  console.log("trying to show you some siiiick pokes");
+  Pokemon.find({}).then(pokemons => {
+    res.json(pokemons)
+  })
 })
