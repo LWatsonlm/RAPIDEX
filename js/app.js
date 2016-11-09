10ec356018f6655b8139419bfcfefdff7e79d0cf
Latoya Watson <latoyamwatson@gmail.com>
2016-11-09 11:40:31 -0500

    angular is connected, index and show pages working as expected

--- js/app.js
+++ js/app.js
@@ -1,2 +1,38 @@
-angular.
-  .module
+angular
+  .module("rapidex", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("PokeIndexController", [
+    PokeIndexControllerFunction
+  ])
+  .controller("PokeShowController", [
+    PokeShowControllerFunction
+  ])
+
+  function RouterFunction($stateProvider) {
+    $stateProvider
+    .state("pokeIndex", {
+      url: "/",
+      templateUrl: "js/ng-views/index.html",
+      controller: "PokeIndexController",
+      controllerAs: "vm"
+    })
+    .state("pokeShow", {
+      url: "/pokemon/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "PokeShowController",
+      controllerAs: "vm"
+    })
+  }
+
+  function PokeIndexControllerFunction() {
+    console.log("i'm in your index");
+  }
+
+  function PokeShowControllerFunction() {
+    console.log("I'm in your show ");
+  }
