5163f069bf80685eb45152fc45107997c79a9f44
Tim Foley <thetimfoley@gmail.com>
2016-11-11 15:35:27 -0500

    add comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,12 @@
 
+// Vue is all about organizing code into "components"
+// this 'pokemon' component lets us drop <pokemon pokemon="bulbasaur"></pokemon> tags into our HTML
 Vue.component("pokemon", {
-  props: ['pokemon'], //  data is passing down to child components using props
+  props: ['pokemon'], //  props let you pass in dynamic values into a component (see `pokemon="bulbasaur"` above.)
+
+  // define your html here. Vue doesn't want you to link to external files, unless you're using build tools
   template: `<div class="pokemon">
-              <img :src="pokemon.sprites.front_default">
+              <img :src="pokemon.sprites.front_default"
               <h3>{{pokemon.name}}</h3>
               <div class="poketypes">
                 <span :class="type.type.name" v-for="type in pokemon.types">{{type.type.name}} </span>
@@ -10,7 +14,9 @@ Vue.component("pokemon", {
             </div>`,
 })
 
+// our index is set up as a component as well. For some reason it's defined slightly differently, but it acts like a component I think...
 const Index = {
+  // check out `v-for` below which is similar to `ng-repeat` in angular
   template: `<div class="pokemonIndex">
                 <h2>CHECK THESE POKéS</h2>
                 <div class="pokeContainer">
@@ -18,15 +24,18 @@ const Index = {
                 </div>
               </div>`,
 
+  // this is where you tell the component what data it has access to
   data: function(){
     return {pokemon: []}
   },
 
+  // a "hook" specifying code to run when the component is "mounted" (i.e. connected) to the DOM
   mounted: function(){
     var resource = this.$resource('/api/pokemon{/id}');
     this.fetchPokemon(resource)
   },
 
+  // define component methods here
   methods: {
     fetchPokemon: function(resource) {
       resource.get().then(function(pokemon) {
@@ -36,6 +45,7 @@ const Index = {
     }
   },
 
+  // define variables that must be dynamically updated
   computed: {
     orderedPokemon: function() {
       return this.pokemon.sort((a, b) => {
@@ -52,19 +62,24 @@ const Index = {
 
 }
 
+// define the Show component, similar to a controller in Angular
 const Show = {
-  template: "<div class='show'> <h2>SHOW</h2> <pokemon pokemon='bulbasuar'></pokemon> <p>{{$route.params.id}}</p></div>",
+  template: "<div class='show'> <h2>SHOW</h2><p>{{$route.params.id}}</p></div>",
 }
 
+// set the routes
 const routes = [
   { path: '/', component: Index},
   { path: '/pokemon/:id', component: Show}
 ]
 
+// set the router
 const router = new VueRouter({
   routes //short for routes: routes
 })
 
+
+// initializing our app!
 const app = new Vue({
   router: router,
 }).$mount("#rapidex")
